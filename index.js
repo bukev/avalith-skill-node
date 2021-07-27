bd255a2ed11959e249c3bcb5ad6fa1e0692207a2
Facundo Nicolás Midvetkin Tourn <44300399+bukev@users.noreply.github.com>
2021-07-27 16:35:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,15 +11,12 @@ app.listen(port, () => {
 
 
 // ----- Obtener fecha ----- //
-
 app.get('/date', (req, res) => {
     res.send(new Date(Date.now()))
 })
 
 
-
 // ----- Leer archivo txt ----- //
-
 app.get('/txt', (req, res) => {
     fs.readFile('text.txt', 'utf8', (err, data) => {
         res.send(data)
@@ -27,10 +24,9 @@ app.get('/txt', (req, res) => {
 })
 
 
-
 // ----- 'Buenos días X!' ----- //
-
 app.post('/hello', (req, res) => {
     console.log(req.body.name)
     res.send(`Buenos días ${req.body.name}!`)
 })
+
